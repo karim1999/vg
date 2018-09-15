5853c8f28ca483313ac1a99c7b9fd4bc249ded46
Karim Mahmoud <karim@Karims-Mac.local>
2018-09-15 15:20:13 -0700

    notifications fixed for ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Karim Mahmoud on 9/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
